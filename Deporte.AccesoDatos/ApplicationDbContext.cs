f3da346547388bb26418bf0a8213b510dab591cc
Elmer Rivadeneira <51166398+elmer2210@users.noreply.github.com>
2020-08-10 17:36:17 -0500

    Add files via upload

---
+++ Deporte.AccesoDatos/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Deportes.CentroDeportivo.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Deportes.AccesoDatos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appConfig.json")
+                .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        }
+        public DbSet<Deporte> Deportes { get; set; }
+        public DbSet<Club> Clubes { get; set; }
+        public DbSet<Equipo> Equipos { get; set; }
+        public DbSet<DeporteClub> DeporteClubes { get; set; }
+        public DbSet<Tecnico> Tecnicos { get; set; }
+        public DbSet<Jugador> Jugadores { get; set; }
+    }
+}
---
+++ Deporte.AccesoDatos/Migrations/20200806081415_Inicio.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Deportes.AccesoDatos.Migrations
+{
+    public partial class Inicio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clubes",
+                columns: table => new
+                {
+                    ClubID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clubes", x => x.ClubID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Deportes",
+                columns: table => new
+                {
+                    DeporteId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreDeporte = table.Column<string>(nullable: true),
+                    TipoDeporte = table.Column<string>(nullable: true),
+                    NumeroJugadores = table.Column<int>(nullable: false),
+                    TiempoJuego = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deportes", x => x.DeporteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tecnicos",
+                columns: table => new
+                {
+                    TecnicoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PrimerNombre = table.Column<string>(nullable: true),
+                    SegundoNombre = table.Column<string>(nullable: true),
+                    ApellidoPaterno = table.Column<string>(nullable: true),
+                    ApellidoMaterno = table.Column<string>(nullable: true),
+                    NombreComppleto = table.Column<string>(nullable: true),
+                    Edad = table.Column<int>(nullable: false),
+                    FechaCreacion = table.Column<DateTime>(nullable: false),
+                    AñosProfesion = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tecnicos", x => x.TecnicoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeporteClubes",
+                columns: table => new
+                {
+                    DetallesId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DeporteId = table.Column<int>(nullable: false),
+                    ClubId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeporteClubes", x => x.DetallesId);
+                    table.ForeignKey(
+                        name: "FK_DeporteClubes_Clubes_ClubId",
+                        column: x => x.ClubId,
+                        principalTable: "Clubes",
+                        principalColumn: "ClubID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DeporteClubes_Deportes_DeporteId",
+                        column: x => x.DeporteId,
+                        principalTable: "Deportes",
+                        principalColumn: "DeporteId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipos",
+                columns: table => new
+                {
+                    EquipoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreEquipo = table.Column<string>(nullable: true),
+                    IntegrantesEquipo = table.Column<int>(nullable: false),
+                    DeporteId = table.Column<int>(nullable: false),
+                    TecnicoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipos", x => x.EquipoId);
+                    table.ForeignKey(
+                        name: "FK_Equipos_Deportes_DeporteId",
+                        column: x => x.DeporteId,
+                        principalTable: "Deportes",
+                        principalColumn: "DeporteId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Equipos_Tecnicos_TecnicoId",
+                        column: x => x.TecnicoId,
+                        principalTable: "Tecnicos",
+                        principalColumn: "TecnicoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jugadores",
+                columns: table => new
+                {
+                    JugadorId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PrimerNombre = table.Column<string>(nullable: true),
+                    SegundoNombre = table.Column<string>(nullable: true),
+                    ApellidoPaterno = table.Column<string>(nullable: true),
+                    ApellidoMaterno = table.Column<string>(nullable: true),
+                    NombreComppleto = table.Column<string>(nullable: true),
+                    Edad = table.Column<int>(nullable: false),
+                    FechaCreacion = table.Column<DateTime>(nullable: false),
+                    Estatura = table.Column<float>(nullable: false),
+                    Posicion = table.Column<string>(nullable: true),
+                    EquipoId = table.Column<int>(nullable: false),
+                    TecnicoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jugadores", x => x.JugadorId);
+                    table.ForeignKey(
+                        name: "FK_Jugadores_Equipos_EquipoId",
+                        column: x => x.EquipoId,
+                        principalTable: "Equipos",
+                        principalColumn: "EquipoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Jugadores_Tecnicos_TecnicoId",
+                        column: x => x.TecnicoId,
+                        principalTable: "Tecnicos",
+                        principalColumn: "TecnicoId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeporteClubes_ClubId",
+                table: "DeporteClubes",
+                column: "ClubId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeporteClubes_DeporteId",
+                table: "DeporteClubes",
+                column: "DeporteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equipos_DeporteId",
+                table: "Equipos",
+                column: "DeporteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equipos_TecnicoId",
+                table: "Equipos",
+                column: "TecnicoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jugadores_EquipoId",
+                table: "Jugadores",
+                column: "EquipoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jugadores_TecnicoId",
+                table: "Jugadores",
+                column: "TecnicoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeporteClubes");
+
+            migrationBuilder.DropTable(
+                name: "Jugadores");
+
+            migrationBuilder.DropTable(
+                name: "Clubes");
+
+            migrationBuilder.DropTable(
+                name: "Equipos");
+
+            migrationBuilder.DropTable(
+                name: "Deportes");
+
+            migrationBuilder.DropTable(
+                name: "Tecnicos");
+        }
+    }
+}
